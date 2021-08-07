bd5b05f4105ba68ee558f2f6bf7b5376a2084f57
fladeia <252842.5s@gmail.com>
2021-08-07 08:09:11 -0300

    done

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 https://courses.wesbos.com/account/access/610d6d64b36fe451adb8914b/view/194130650
-Day 1
+Javascript30 - 30 days vanilla js coding challenge
+build 30 things in 30 days with 30 tutorials
 
+Day 1 - Javascript Drum Kit
+
+original github repo:
+https://github.com/wesbos/JavaScript30/tree/master/01%20-%20JavaScript%20Drum%20Kit
+
+interesting points:
+HTML - tags: kdb, audio
+HTML - attributes: data-key
+CSS: `transition: all 0.07s ease;`, `transform: scale(1.1);`
+Javascript: `window.addEventListener("keydown", () => {})`, audio methods, classList add and remove
+
+references:
 https://keycode.info/
-
-search
-
-html tag:
-kbd
-audio
-
-html tag attribute:
-data-
-
-12:26
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-window.addEventListener("keydown", (e) => {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) {
-    return;
-  }
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skip it if it's not a transfomr
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+window.addEventListener("keydown", playSound);
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
